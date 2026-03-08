a1355eade9f959bc58c5334fc9421c2850e031d6
hayshin <hayshinbj@gmail.com>
2026-03-08 13:36:22 +0500

    chore: add docker image to nix flake

--- flake.nix
+++ flake.nix
@@ -12,9 +12,22 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       naersk = pkgs.callPackage naersk-package {};
-    in {
-      defaultPackage = naersk.buildPackage {
+      tagbot = naersk.buildPackage {
         src = ./.;
       };
+    in {
+      packages = {
+        default = tagbot;
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "tagbot";
+          tag = "latest";
+          contents = [ pkgs.cacert ];
+          config = {
+            Cmd = [ "${tagbot}/bin/tagbot" ];
+            Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+          };
+        };
+      };
+      defaultPackage = self.packages.${system}.default;
     });
 }
